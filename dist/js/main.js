35f025b1840224aea83caebfb79267eabc8da217
tyson <tyson22509@hotmail.com>
2019-07-22 11:16:13 -0400

    menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -20,11 +20,14 @@ function toggleMenu() {
 
         // Set the menu state
         showMenu = true;
+
     } else {
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
+
+        showMenu = false;
     }
 }
